6441278c9514da7652f97f148af6911cfce5c539
Marlon-Somers <9021883@student.zadkine.nl>
2023-12-15 00:43:48 +0100

    added collor change with a only a little help

--- opdracht-3-div bewegen/app.js
+++ opdracht-3-div bewegen/app.js
@@ -1,19 +1,26 @@
 var div = document.getElementById('animatedDiv');
 var currentPosition = 0;
 var targetPosition = 1450;
-var speed = 5; 
-
+var speed = 3 ;
 function animate() {
   currentPosition += speed;
 
 
   if (currentPosition >= targetPosition || currentPosition <= 0) {
     speed = -speed;
+    changeColor();
   }
 
   div.style.left = currentPosition + 'px';
 
   requestAnimationFrame(animate);
 }
+
+function changeColor() {
+  var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  div.style.backgroundColor = randomColor;
+}
+
+div.addEventListener('click', changeColor);
 
 animate();
